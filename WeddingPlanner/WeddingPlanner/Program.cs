bc5cff6b755d654037a130eb4991eafe0e9ddf79
Tarkol <connorgeorgiou@gmail.com>
2014-02-21 12:14:48 +0000

    First working reservation.

--- WeddingPlanner/WeddingPlanner/Program.cs
+++ WeddingPlanner/WeddingPlanner/Program.cs
@@ -5,38 +5,104 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Xml;
+using System.IO;
 
 namespace COMP28112ex2
 {
     class Program
     {
-        static int requestID = 1;
-        static String username = "butts";
-        static String password = "morebutts";
+        static int requestID;
+        static String username;
+        static String password;
+        static String urlBand;
+        static String urlHotel;
 
         static void Main(string[] args)
         {
-            var client = new WebClient();
-            String test = client.DownloadString("http://www.mspaintadventures.com");
-            setReservation(1);
-            Console.In.Read();
+            bool fileOK = true;
+            try
+            {
+                readSettingsFile();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error reading settings file: " + e.Message);
+                fileOK = false;
+            }
+            if (fileOK)
+            {
+                try
+                {
+                    setReservation(44);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Unknown HTTP Response: " + e.Message);
+                }
+                Console.In.Read();
+            }
+        }
+
+        static void readSettingsFile()
+        {
+            String[] settings = File.ReadAllLines("clientsettings.txt");
+            username = settings[0];
+            password = settings[1];
+            requestID = Convert.ToInt32(settings[2]);
+            urlHotel = settings[3];
+            urlBand = settings[4];
+
+            File.WriteAllLines("clientsettings.txt", settings);
+        }
+
+        public static void sendRequest(String xml)
+        {
+            byte[] arr = System.Text.Encoding.UTF8.GetBytes(xml);
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(urlHotel);
+            request.Method = "PUT";
+            request.ContentType = "text/xml";
+            request.Accept = "application/xml";
+            //request.ContentLength = arr.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(arr, 0, arr.Length);
+            dataStream.Close();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                
+
+
+            }
+            else if (response.StatusCode == HttpStatusCode.ServiceUnavailable)
+            {
+                throw new Exception(response.StatusCode + ": " + response.StatusDescription);
+            }
+            else
+                throw new Exception(response.StatusCode.ToString());
+
+            string returnString = response.StatusCode.ToString();
+            Console.WriteLine(returnString);
         }
 
         public static void setReservation(int slotID)
         {
             StringBuilder requestString = new StringBuilder();
-            using (XmlWriter xmlWrite = XmlWriter.Create(requestString))
+            XmlWriterSettings xmlSettings = new XmlWriterSettings();
+            xmlSettings.ConformanceLevel = ConformanceLevel.Fragment;
+            xmlSettings.OmitXmlDeclaration = true;
+            XmlWriter xmlWrite = XmlWriter.Create(requestString, xmlSettings);
+           
+            using (xmlWrite)
             {
-                xmlWrite.WriteStartDocument();
                 xmlWrite.WriteStartElement("reserve");
                 xmlWrite.WriteElementString("request_id", requestID.ToString());
                 xmlWrite.WriteElementString("username", username);
                 xmlWrite.WriteElementString("password", password);
                 xmlWrite.WriteElementString("slot_id", slotID.ToString());
                 xmlWrite.WriteEndElement();
-                xmlWrite.WriteEndDocument();
             }
             Console.Out.WriteLine(requestString);
+            sendRequest(requestString.ToString());
         }
 
         //public abstract void clearReservation();
